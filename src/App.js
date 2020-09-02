c4a6cb87681f1e57e8ff80f79a866172caa07e43
eze <emollinedo@untref.edu.ar>
2020-09-02 18:04:14 -0300

    Hasta cambiar estado de completado

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import './App.css';
 import Todos from './components/Todos.js';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: true
+      }
+    ]
+  }
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos:  this.state.todos.map(todo => {
+      if( todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
