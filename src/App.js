8eec12bcfc80f16cc4bb0376081b63d824310442
Unknown <emollinedo@untref.edu.ar>
2020-09-04 16:43:40 -0300

    agrego funcionalidad de agregar todos, empiezo con router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Header from './components/layout/Header.js';
 import './App.css';
 import Todos from './components/Todos.js';
 import AddTodo from './components/AddTodo.js';
+import {v4 as uuid} from 'uuid';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid(),
         title: 'Meeting with boss',
         completed: true
       }
@@ -39,16 +42,28 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
   }
 
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="container">
-          <Header />
-          <AddTodo />
-          <Todos todos={this.state.todos} markComplete={this.markComplete}
-            delTodo={this.delTodo} />
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <AddTodo addTodo={this.addTodo} />
+            <Todos todos={this.state.todos} markComplete={this.markComplete}
+              delTodo={this.delTodo} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
